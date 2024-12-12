3bd9a5f53b1076e5d26376fff16efb549d967e1f
portfolio_dimas <dimasbintangpratama5@gmail.com>
2024-12-12 15:48:38 +0700

    dashboard

--- src/layout/layoutProject.tsx
+++ src/layout/layoutProject.tsx
@@ -6,7 +6,7 @@ const layoutProject: React.FC<Props> = ({ children, bgColor = "bg-white" }) => {
   return (
     <>
       <div
-        className={`justify-between w-full overflow-x-auto rounded-lg min-h-96 card text-slate-800 ${bgColor}`}
+        className={`justify-between w-full overflow-x-auto rounded-lg  text-slate-800 ${bgColor}`}
       >
         {children}
       </div>
