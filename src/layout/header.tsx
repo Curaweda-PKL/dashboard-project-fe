c7d50f6583f0f74cf0971e55db5bb2f37ce9f98a
ithfaith <mochammadfaith10@gmail.com>
2025-01-17 15:40:28 +0700

    Update Login page

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -162,7 +162,7 @@ const Layout = () => {
                     </div>
                     <div className="text-right mr-4">
                       <p className="text-sm font-semibold lg:text-base">
-                        Mochammad Faith
+                        Username
                       </p>
                       <p className="text-xs">axyz@gmail</p>
                     </div>
