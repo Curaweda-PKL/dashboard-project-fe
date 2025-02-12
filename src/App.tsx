c4757ded6f772eb15e984d6bfb404154dda5f1f0
ithfaith <mochammadfaith10@gmail.com>
2025-02-12 09:17:09 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layout/header";
 import Dashboard from "./pages/dashboard";
 import TeamTable from "./pages/Team";
-import LoginPage from "./pages/auth/loginPage";
+import LoginPage from "./pages/Auth/loginPage";
 import LoginLayout from "./layout/layoutLogin";
 import NotFound from "./pages/notFound";
 import Setting from "./pages/Setting";
